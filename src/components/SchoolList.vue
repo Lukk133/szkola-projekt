8a21d93ad1c5fa4f3f62e98055a91365e8e9525b
Lukk133 <zielinskil@outlook.com>
2023-10-30 18:39:40 +0100

    added functioning edit button to schoolList

--- src/components/SchoolList.vue
+++ src/components/SchoolList.vue
@@ -34,16 +34,37 @@
  
   <tbody>
   
-      <tr v-for=" school in schools" :key="school.name">
+      <tr v-for=" (school, index) in schools" :key="school.name">
         <td>{{ school.id }}</td>
         <td>
       <RouterLink  :to="{ name: 'SchoolPage', params: { id:school.name } }">
-            <h2><div type="input" @click="goToSchoolPage()" class="links">{{ school.name }}</div></h2>
+            <h2><div type="input" @click="goToSchoolPage()" class="links">{{ school.name }}</div>
+            </h2>
             <!--textArea-->
           </RouterLink>
         </td>
-        <v-icon  @clic="editSchoolName(index)" icon="fa fa-edit pt-2"/>
-        <v-icon @click="deleteSchool(index)" icon="fa fa-trash pl-5 pt-2"/>
+        <v-dialog @click:outside="closeDialog, clearEdit()" height="450" width="500">
+  <template v-slot:activator="{ props }">
+    <v-icon  v-bind="props" @click="editSchoolName(index)" icon="fa fa-edit pr-5 mt-2"/>
+  </template>
+  <template v-slot:default="{ isActive }">
+    <v-card title="Edytuj nazwę szkoły" >
+      <v-card-text >
+        <v-text-field v-model="schoolName"></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+        size="large"
+        class="ma-3 ml-4 mb-10"
+          text="Edytuj"
+          @click="isActive.value = false, addSchool(index)"
+        ></v-btn>
+      </v-card-actions>
+    </v-card>
+  </template>
+</v-dialog>
+        <v-icon @click="deleteSchool(index)" icon="fa fa-trash pl-5 mt-2"/>
       </tr>
     </tbody>
   </v-table>
@@ -51,9 +72,13 @@
 
 <script>
 export default {
+  name: 'HelloWorld',
   data(){
     return{
+      schoolName: '',
       schoolNumber: 5,
+      editedTask: null,
+      editedSchoolName: '',
       schools: [
           {
             id: 1,
@@ -86,20 +111,29 @@ export default {
         this.$router.push(`/${this.schools.name}`);
       },
       addSchool(){
-        this.schoolNumber += 1
+        if(this.editedTask === null){
+          this.schoolNumber += 1
         this.schools.push({
           id: this.schoolNumber,
           name: this.schoolName
-          })
+          }) 
+        } else {
+          this.schools[this.editedTask].name = this.schoolName
+        this.editedTask = null
+        }
         this.schoolName = ''
       },
       deleteSchool(index){
       this.schools.splice(index, 1)
     },
     editSchoolName(index){
-      this.schools = this.schoolName[index].name
+      this.schoolName = this.schools[index].name
       this.editedTask = index
     },
+    clearEdit(){   
+        this.schoolName = '',
+        this.editedTask = null
+    }
     }
   }
 
